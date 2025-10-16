1c785fad42bd7da48cb8a0c034184a9ff8fcf0b5
bigroot <88224277+BigRoot-K@users.noreply.github.com>
2025-10-16 02:11:50 +0000

    post 요청시 파일생성하는 로직을 적용해봄

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 경로를 생략하면 http라는 글로벌 모듈을 찾는다.
 const http = require('http');
-const fs = require('file');
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
 
@@ -18,18 +18,21 @@ const server = http.createServer( (req, res) => {
 	//process.exit(); // 서버를 종료한다.
 	console.log("method ::  " + method );
 	if(url === '/message' && method === 'POST'){
-		//commit test
-		//응답의 설명서(헤더)를 작성하는 도구
-		res.setHeader('Content-Type', 'text/html'); //응답 헤더 설정
-		res.write('<html>');
-		res.write('<head><title>My First Page</title></head>');
-		res.write('<body><h1>Hello from my Node.js Server22!</h1></body>');
-		res.write('</html>');
-
-		res.end(); //응답을 종료한다.
-
+		fs.writeFileSync('message.txt', 'DUMMY');
+		res.statusCode = 302 ; 
+		res.setHeader('Location',  '/');
+		return res.end();
 	}
 
+	//commit test
+	//응답의 설명서(헤더)를 작성하는 도구
+  	res.setHeader('Content-Type', 'text/html'); //응답 헤더 설정
+	res.write('<html>');
+	res.write('<head><title>My First Page</title></head>');
+	res.write('<body><h1>Hello from my Node.js Server22!</h1></body>');
+	res.write('</html>');
+
+	res.end(); //응답을 종료한다.
 
 	
 }); //서버를 생성 
